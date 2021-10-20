daaacc34f2b1a048fb41108c55b633996c3c8b62
Corey Reid <91506947+Corey-R@users.noreply.github.com>
2021-10-20 17:38:47 -0500

    Created First Javascript File
    Made two new directories within the Basic Javascript Projects repository containing an index.html file and an external js file (contained within the JS directory)

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,72 @@
+// Creating an alert box with string value
+window.alert("I'm not gonna say it again...Hello, World!");
+
+// Displaying string value inside Project 1 document
+document.write('Hello, World!');
+
+// Defining variable A a string value
+var A = " Sorry for the late reply!";
+
+// Displaying newly defined variable A into Project 1 document
+document.write(A);
+
+// Assigning variable X a string value
+var X = "This is the string assignment";
+
+// Creating a second alert box to display 
+// the string value assined to variable X
+window.alert(X);
+
+// Using the backslash to escape characters of 
+// quotation marks and apostrophes so they will 
+// be displayed within the Project 1 document
+document.write( " Peter asked, \"Oh, have you not heard?\" \"Heard what?\" asked Bryan. Stewie\: \"Nooooo...\" ");
+
+// Using the plus sign to concatenate or 
+// "put together" two separate string values
+// to combine them
+document.write(" This text includes" + " concatenated string!");
+
+// Setting variable C with concatenated string
+// values while using the backslash to display
+// the 's in Let's
+var C = " Let\'s concatenate this next text\:" + " concatenate completed!";
+
+// Using this syntax to display the value of variable
+// C in the Project 1 document
+document.write(C);
+
+// Assigning multiple variables within the same statement.
+// Note: statements are separate by semicolon(s) ";" and the 
+// commas separate each variable. The equal sign assigns each
+// variable (i.e. Family "=") with their following value 
+// (i.e. "The Griffins")
+var Family = " The Griffins", Mom = " Lois", Dad = " Peter", Dog = " Bryan", Son = " Chris", Daughter = " Meg ", Baby = " Stewie";
+
+// For each variable, we assigned various colors to their string
+// value by using the method.
+
+// As a side note, methods are pre-made instructions WITHIN AN OBJECT to change the state
+// of the SAME OBJECT it's defined in.
+
+// Note: each variable has already been assigned their string value above
+// so there is no need to reassign their values unless chosen to do so.
+var Family = Family.fontcolor("red");
+var Mom = Mom.fontcolor("green");
+var Dad = Dad.fontcolor("brown");
+var Dog = Dog.fontcolor("orange");
+var Son = Son.fontcolor("blue");
+var Daughter = Daughter.fontcolor("orchid");
+var Baby = Baby.fontcolor("pink");
+
+// Selecting one of the above variables to be displayed in
+// the Project 1 document
+document.write(Daughter);
+
+// Displaying the results of the expression "12 / 3" in the
+// Project 1 document.
+document.write(12 / 3);
+
+
+
+
